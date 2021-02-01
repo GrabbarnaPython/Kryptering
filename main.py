7e9943a1852b2513524aa84b5793465f92c0279f
Linus Zetterman <20lize@stockholmscience.se>
2021-02-01 15:37:16 +0000

    some stuff

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from encrypt import *
-
 what=input("main or encrypt? ")
 
 if what=="main":
 	coords = []
 	x = int(input("x? "))
 	y = int(input("y? "))
-	
-	text = input("Krypterad text: ")
-	lista = []
 
-	for i in range(text):
-		lista.append(0)
+	text = input("Krypterad text: ")
 
 	for i in range(x):
 			coords.append([])
@@ -24,8 +18,7 @@ if what=="main":
 	xIncrease = 1
 	yIncrease = 1
 	t=0
-	
-	code = []
+
 	for p in range(len(text)):
 		if coords[xPos][yPos] == '*':
 			coords[xPos][yPos] = t
@@ -45,21 +38,15 @@ if what=="main":
 
 	for j in range(len(coords)):
 		print(coords[j])
+	code=[]
+	realcode=""
 	for q in range(x):
 			for r in range(y):
 					if coords[q][r] != '*':
 						code.append(coords[q][r])
 	print(code)
-	v = 0
 	for w in code:
-		print(text[w])
-		lista[w] = text[v]
-		v += 1
+		realcode+=text[w]
 	print(realcode)
 else:
-  encrypt()
-
-realcode = ''
-
-for i in range(len(list)):
-	realcode += i
+	encrypt()
